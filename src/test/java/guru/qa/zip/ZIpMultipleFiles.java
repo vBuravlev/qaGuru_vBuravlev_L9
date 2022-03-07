1923ffab216c5cea2e17668c4a6cb54d1077f99d
Vadim Buravlev <masta54@mail.ru>
2022-03-07 09:49:02 +0700

    upgrade method zipFileCreate

--- src/test/java/guru/qa/zip/ZIpMultipleFiles.java
+++ src/test/java/guru/qa/zip/ZIpMultipleFiles.java
@@ -1,24 +1,19 @@
 package guru.qa.zip;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
+import java.util.zip.*;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ZIpMultipleFiles {
@@ -35,40 +30,40 @@ public class ZIpMultipleFiles {
         assertThat(srcFiles, is(not(empty())));
 
         //Создание потока для zip файла
-        FileOutputStream fos = new FileOutputStream(resourcesDirName + zipFileName);
-        ZipOutputStream zipOut = new ZipOutputStream(fos);
 
-        //Добавление файлов из директории в zip файл
-        for (String srcFile : srcFiles) {
-            File fileToZip = new File(classLoader.getResource(srcFile).getFile());
-            FileInputStream fis = new FileInputStream(fileToZip);
-            ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-            zipOut.putNextEntry(zipEntry);
+        try {
+            ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(resourcesDirName + zipFileName));
+            zipOut.setLevel(Deflater.BEST_SPEED);
+            //Добавление файлов из директории в zip файл
+            for (String srcFile : srcFiles) {
+                File fileToZip = new File(classLoader.getResource(srcFile).getFile());
+                FileInputStream fis = new FileInputStream(fileToZip);
+                ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+                zipOut.putNextEntry(zipEntry);
 
-            byte[] bytes = new byte[1024];
-            int length;
-            while ((length = fis.read(bytes)) >= 0) {
-                zipOut.write(bytes, 0, length);
+                byte[] bytes = new byte[1024];
+                int length;
+                while ((length = fis.read(bytes)) >= 0) {
+                    zipOut.write(bytes, 0, length);
+                }
+                fis.close();
             }
-            fis.close();
+            zipOut.close();
+
+            //Сравнение содержимого zip файла и списка файлов в каталоге
+            ZipFile zf = new ZipFile(resourcesDirName + zipFileName);
+            Enumeration en = zf.entries();
+            List<String> list = Collections.list(en);
+            assertEquals(list.toString(), srcFiles.toString());
+            zf.close();
+
+            //Удаление файла после прохождения теста
+            Files.delete(Paths.get(resourcesDirName + zipFileName));
+
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
         }
-        zipOut.close();
-        fos.close();
 
-        //Получение списка файлов с zip файлом
-        List<String> srcFilesZip = FilesToList.filesToList(resourcesDirName);
-
-        //Проверка, что файл zip существуют в дирректории
-        assertThat(srcFilesZip, hasItem(zipFileName));
-
-        //Удаление файла после прохождения теста
-        Files.delete(Paths.get(resourcesDirName + zipFileName));
-
-        //Получение списка файлов без zip файла
-        List<String> srcFilesNotZip = FilesToList.filesToList(resourcesDirName);
-
-        //Проверка, что файл zip отсутствует в дирректории
-        assertThat(srcFilesNotZip, hasSize(srcFilesZip.size()-1));
     }
 
 
